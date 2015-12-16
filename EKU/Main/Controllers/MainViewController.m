a1cf86df6baf496d1780944bb8127a8a31a9169e
shen <852700979@qq.com>
2015-12-16 15:47:46 +0800

    首页样式

--- EKU/Main/Controllers/MainViewController.m
+++ EKU/Main/Controllers/MainViewController.m
@@ -10,10 +10,12 @@
 #import "EKUMineTopSearchBar.h"
 #import "EKUMineTopVideosView.h"
 #import "EKUMineCompetitiveVideoView.h"
+#import "EKUBottomTestView.h"
 @interface MainViewController()<EKUMineTopSearchBarDelegate>
 @property (nonatomic,strong) EKUMineTopSearchBar *topSearchBar;
 @property (nonatomic,strong) EKUMineTopVideosView *topVideoView;
 @property (nonatomic,strong) EKUMineCompetitiveVideoView *middleVideoView;
+@property (nonatomic,strong) EKUBottomTestView *bottomTest;
 @end
 @implementation MainViewController
 -(void) viewDidLoad
@@ -35,6 +37,9 @@
     
     _middleVideoView = [[EKUMineCompetitiveVideoView alloc]initWithFrame:CGRectMake(0, TOPHEIGHT+45+SCREENHEIGHT*0.25, SCREENWIDTH,  SCREENHEIGHT*0.3)];
     [self.view addSubview:_middleVideoView];
+    
+    _bottomTest = [[EKUBottomTestView alloc]initWithFrame:CGRectMake(0, _middleVideoView.frame.origin.y+SCREENHEIGHT*0.3, SCREENWIDTH, SCREENHEIGHT-(_middleVideoView.frame.origin.y+SCREENHEIGHT*0.3))];
+    [self.view addSubview:_bottomTest];
     self.view.backgroundColor = [UIColor  greenColor];
 }
 //
--- EKU/Main/Controllers/Views/EKUBottomTestView.m
+++ EKU/Main/Controllers/Views/EKUBottomTestView.m
@@ -11,8 +11,34 @@
 @implementation EKUBottomTestView
 -(instancetype)initWithFrame:(CGRect)frame{
     if (self = [super initWithFrame:frame]) {
-        
+        [self configureUI];
     }
     return self;
+}
+-(void)configureUI{
+    CGFloat viewWidth = self.frame.size.width;
+    CGFloat viewHeight = self.frame.size.height;
+    
+    UIView *topContentView = [[UIView alloc]initWithFrame:CGRectMake(0,0,viewWidth,viewHeight*0.25)];
+    topContentView.backgroundColor = [UIColor redColor];
+    UIView *bottomContentView =[[UIView alloc]initWithFrame:CGRectMake(0 ,viewHeight*0.25, viewWidth, viewHeight*0.75)];
+    bottomContentView.backgroundColor =[UIColor purpleColor];
+    
+    UILabel *titleLabel = [[UILabel alloc]initWithFrame:CGRectMake(0, 0, 80,viewHeight*0.25)];
+    titleLabel.text = @"最新试题";
+    UIButton *moreButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    moreButton.frame = CGRectMake(viewWidth-40, 0, 40, 30);
+    [moreButton setTitle:@"更多" forState:UIControlStateNormal];
+    [topContentView addSubview:titleLabel];
+    [topContentView addSubview:moreButton];
+    
+    for (int i = 0; i<4; i++) {
+        UIButton *testItem = [UIButton buttonWithType:UIButtonTypeSystem];
+        testItem.frame = CGRectMake(2+viewWidth/4*i, 0, viewWidth/4-5, viewHeight*0.75);
+        testItem.backgroundColor = [UIColor yellowColor];
+        [bottomContentView addSubview:testItem];
+    }
+    [self addSubview:topContentView];
+    [self addSubview:bottomContentView];
 }
 @end
