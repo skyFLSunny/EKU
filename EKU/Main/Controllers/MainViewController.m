65019836d778bbbf74e7b06774d3ad083fab8768
shen <852700979@qq.com>
2015-12-18 10:33:46 +0800

    scrollview

--- EKU/Main/Controllers/MainViewController.m
+++ EKU/Main/Controllers/MainViewController.m
@@ -16,30 +16,38 @@
 @property (nonatomic,strong) EKUMineTopVideosView *topVideoView;
 @property (nonatomic,strong) EKUMineCompetitiveVideoView *middleVideoView;
 @property (nonatomic,strong) EKUBottomTestView *bottomTest;
+@property (nonatomic,strong) UIScrollView *bgScrollView;
 @end
 @implementation MainViewController
 -(void) viewDidLoad
 {
     self.title =@"首页";
+    self.automaticallyAdjustsScrollViewInsets =NO;
     [super viewDidLoad];
+    UIDevice *d =[UIDevice currentDevice];
+    [d description];
     [self setStyle];
 }
 
 // 设置样式
 -(void) setStyle
 {
-    _topSearchBar = [[EKUMineTopSearchBar alloc]initWithFrame:CGRectMake(0, TOPHEIGHT, SCREENWIDTH, 45)];
+    _bgScrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, TOPHEIGHT, SCREENWIDTH, SCREENHEIGHT-FOOTHIGHT-TOPHEIGHT)];
+    _bgScrollView.backgroundColor = [UIColor whiteColor];
+    _bgScrollView.contentSize = CGSizeMake(SCREENWIDTH, SCREENHEIGHT-FOOTHIGHT);
+    [self.view addSubview:_bgScrollView];
+    _topSearchBar = [[EKUMineTopSearchBar alloc]initWithFrame:CGRectMake(0, 0, SCREENWIDTH, 45)];
     _topSearchBar.delegate = self;
-    [self.view addSubview:_topSearchBar];
+    [_bgScrollView addSubview:_topSearchBar];
     
-    _topVideoView = [[EKUMineTopVideosView alloc]initWithFrame:CGRectMake(0, TOPHEIGHT+45, SCREENWIDTH, SCREENHEIGHT*0.25)];
-    [self.view addSubview:_topVideoView];
+    _topVideoView = [[EKUMineTopVideosView alloc]initWithFrame:CGRectMake(0, 45, SCREENWIDTH, SCREENHEIGHT*0.25)];
+    [_bgScrollView addSubview:_topVideoView];
     
-    _middleVideoView = [[EKUMineCompetitiveVideoView alloc]initWithFrame:CGRectMake(0, TOPHEIGHT+45+SCREENHEIGHT*0.25, SCREENWIDTH,  SCREENHEIGHT*0.3)];
-    [self.view addSubview:_middleVideoView];
+    _middleVideoView = [[EKUMineCompetitiveVideoView alloc]initWithFrame:CGRectMake(0, 45+SCREENHEIGHT*0.25, SCREENWIDTH,  SCREENHEIGHT*0.3)];
+    [_bgScrollView addSubview:_middleVideoView];
     
-    _bottomTest = [[EKUBottomTestView alloc]initWithFrame:CGRectMake(0, _middleVideoView.frame.origin.y+SCREENHEIGHT*0.3, SCREENWIDTH, SCREENHEIGHT-(_middleVideoView.frame.origin.y+SCREENHEIGHT*0.3))];
-    [self.view addSubview:_bottomTest];
+    _bottomTest = [[EKUBottomTestView alloc]initWithFrame:CGRectMake(0, _middleVideoView.frame.origin.y+SCREENHEIGHT*0.3, SCREENWIDTH, SCREENHEIGHT-(_middleVideoView.frame.origin.y+SCREENHEIGHT*0.3)-FOOTHIGHT-TOPHEIGHT)];
+    [_bgScrollView addSubview:_bottomTest];
     self.view.backgroundColor = [UIColor  greenColor];
 }
 //
